a6e6d1a099e3e2af614612971144d2de10523022
midhul <dhulmi3@gmail.com>
2023-06-14 15:10:51 +0530

    Added templates and its functions

--- crawler/web.py
+++ crawler/web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask ,render_template
 
 import models
 
@@ -16,22 +16,37 @@ def index():
 def users(id):
     return f"You asked for user {id}"
 
-@app.route("/artist")
-def artist():
+# @app.route("/artist")
+# def artist():
      
-     db=models.init_db(app)
-     artists=db.session.execute(db.select(models.Artist.name))
-     new="<h1>List of artists</h1>"
-     for artist in artists:
-        #  for i,j in artist:
-        #      new+=i.name
+#      db=models.init_db(app)
+#      artists=db.session.execute(db.select(models.Artist.name))
+#      new="<h1>List of artists</h1>"
+#      for artist in artists:
+#         #  for i,j in artist:
+#         #      new+=i.name
          
 
 
 
-         new += f"<p>->{artist.name}</p>"
-        #  return artists
-     return new
+#          new += f"<p>->{artist.name}</p>"
+#         #  return artists
+#      return new
+
+@app.route("/artist")
+def artist():
+     
+     db=models.init_db(app)
+     artists=db.session.execute(db.select(models.Artist)).scalars() 
+     return render_template("index.html",artists=artists)
+
+@app.route("/artist/<artist_id>")
+def artistid(artist_id):
+     
+     db=models.init_db(app)
+     artists=db.session.execute(db.select(models.Artist).filter(models.Artist.id == artist_id)).scalar()
+     print(artists)
+     return render_template("artists.html",artists=artists, foo="hello")
          
 @app.route("/artist/tracks")
 def artist_tracks():
@@ -42,3 +57,11 @@ def artist_tracks():
     for artist_name, track_name in tracks:
         new += f"<p>{artist_name}=>{track_name}</p>"
     return new
+
+
+
+@app.route("/lyrics/<song_id>")
+def lyrics(song_id):
+    db=models.init_db(app)
+    track=db.session.execute(db.select(models.Tracks).filter(models.Tracks.id==song_id)).scalar()
+    return render_template('track.html',track=track)
